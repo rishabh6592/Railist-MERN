90fd7cbd6212d121f4f22ce3704c5268c77e04b9
rishabh <rishabhkumar6592@gmail.com>
2026-08-24 23:00:20 +0530

    fix: update vite chunk config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,5 +9,17 @@ export default defineConfig({
     proxy: {
       "/api": "http://localhost:5000"
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 1600, // Warning limit badha di (1.6 MB tak warning nahi aayegi)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Packages ko alag bundle me divide karega
+          }
+        }
+      }
+    }
   }
 });
